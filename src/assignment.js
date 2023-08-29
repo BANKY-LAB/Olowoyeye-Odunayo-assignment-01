52cd3020402cb8156d965cd160125e02cc1d443b
Olowoyeye Odunayo <Olowoyeyesnr@gmail.com>
2023-08-29 16:48:45 +0000

    wrote 2 functions to sum of no and sum of even no.

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,13 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
-    return sum
+    for (let i = 0; i <= destination; i++){
+        sum += i;
+    }
+    return sum;
 }
 
-
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -39,6 +41,13 @@ function countEvenNumbersWithin(destination) {
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for (let i = 1; i <= destination; i++) {
+        if (i % 2 === 0) {
+          count++;
+          sum += i;
+          arrayOfEvenNumbers.push(i);
+        }
+      }
     return {
         // property value shorthand
         // when the property name and the value name are the same
@@ -49,7 +58,7 @@ function countEvenNumbersWithin(destination) {
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
